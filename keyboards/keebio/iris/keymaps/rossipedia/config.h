851b24a37f57465034de78dc2f0efe8eac1c9502
Bryan Ross <bryan.ross@reddit.com>
2019-11-11 14:05:14 -0700

    layout updates - add switch for resetting eeprom

--- keyboards/keebio/iris/keymaps/rossipedia/config.h
+++ keyboards/keebio/iris/keymaps/rossipedia/config.h
@@ -19,6 +19,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 // #define USE_I2C
 #define EE_HANDS
+#define TAPPING_TERM 200
+
 
 #undef RGBLED_NUM
 #define RGBLIGHT_ANIMATIONS
--- keyboards/keebio/iris/keymaps/rossipedia/rules.mk
+++ keyboards/keebio/iris/keymaps/rossipedia/rules.mk
@@ -3,3 +3,7 @@ BACKLIGHT_ENABLE = yes
 
 # Always want a way to get to the bootloader.
 BOOTMAGIC_ENABLE = lite
+
+# Trim the fat
+LTO_ENABLE = yes
+TAP_DANCE_ENABLE = yes
