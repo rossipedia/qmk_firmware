7e3cce4289d328064a4494873fc65ac21a5b3576
Bryan Ross <bryan.ross@reddit.com>
2019-11-12 13:54:03 -0700

    use LSFT_T(KC_ENT) for right shift

--- keyboards/keebio/iris/keymaps/rossipedia/keymap.c
+++ keyboards/keebio/iris/keymaps/rossipedia/keymap.c
@@ -17,6 +17,7 @@ enum custom_keycodes {
 
 // Send ESC on tap, CTRL on hold
 #define CTL_ESC CTL_T(KC_ESC)
+#define RS_ENTR RSFT_T(KC_ENT)
 
 // Jesus tap dancing christ
 enum {
@@ -39,7 +40,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
      CTL_ESC, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                               KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_LBRC,          KC_RBRC, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
+     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_LBRC,          KC_RBRC, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, RS_ENTR,
   //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
                                     KC_LALT, TD_OSPC, LOWER,                     RAISE,   KC_SPC,  KC_ENT
                                 // └────────┴────────┴────────┘                 └────────┴────────┴────────┘
