4314e8c5e5b616a195f8eda5742f5395bae7a1ef
Bryan Ross <bryan.ross@reddit.com>
2019-12-19 12:30:46 -0700

    forgot the TD for opt+space in preonic

--- keyboards/preonic/keymaps/rossipedia/keymap.c
+++ keyboards/preonic/keymaps/rossipedia/keymap.c
@@ -65,7 +65,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,  \
   CTL_ESC, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
   KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, RS_ENTR, \
-  OSM_GA,  KC_LCTL, KC_LALT, KC_LGUI,     LOWER    ,      KC_SPC     ,    KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT  \
+  OSM_GA,  KC_LCTL, KC_LALT, TD_OSPC,     LOWER    ,      KC_SPC     ,    KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT  \
 ),
 
 /* Lower
