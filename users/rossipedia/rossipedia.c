ff0fd02226d72ddcabe2511d591d4215ef8a7593
Bryan Ross <git@rossipedia.com>
2019-12-27 19:46:12 -0700

    add users/rossipedia folder

---
+++ users/rossipedia/rossipedia.c
@@ -0,0 +1,8 @@
+#include QMK_KEYBOARD_H
+#include "rossipedia.h"
+
+// Tap Dance Array
+qk_tap_dance_action_t tap_dance_actions[] = {
+    [TD_OPT_SPACE] = ACTION_TAP_DANCE_DOUBLE(KC_LGUI, LALT(KC_SPC))
+};
+
