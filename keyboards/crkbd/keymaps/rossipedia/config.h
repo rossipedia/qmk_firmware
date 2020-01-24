57e2042cb007cd63147a0fc1bdc557d3ac51a4c1
Bryan Ross <bryan.ross@reddit.com>
2020-01-24 15:41:05 -0700

    EE_HANDS and tapping updates for crkbd

--- keyboards/crkbd/keymaps/rossipedia/config.h
+++ keyboards/crkbd/keymaps/rossipedia/config.h
@@ -24,16 +24,16 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 /* Select hand configuration */
 
-#define MASTER_LEFT
+// #define MASTER_LEFT
 // #define MASTER_RIGHT
-// #define EE_HANDS
+#define EE_HANDS
 
 #define SSD1306OLED
 
 #define USE_SERIAL_PD2
 
-#define TAPPING_FORCE_HOLD
-#define TAPPING_TERM 100
+//#define TAPPING_FORCE_HOLD
+#define TAPPING_TERM 200
 
 #ifdef RGBLIGHT_ENABLE
     #undef RGBLED_NUM
