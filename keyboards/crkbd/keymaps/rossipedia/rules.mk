0ac29772cc24cbbf753808b9b4ed7f8e44298eae
Bryan Ross <bryan.ross@reddit.com>
2020-01-25 09:42:54 -0700

    corne updates

--- keyboards/crkbd/keymaps/rossipedia/rules.mk
+++ keyboards/crkbd/keymaps/rossipedia/rules.mk
@@ -2,9 +2,10 @@
 # If you want to change the display of OLED, you need to change here
 SRC +=  ./lib/glcdfont.c \
         ./lib/rgb_state_reader.c \
-        ./lib/layer_state_reader.c \
         ./lib/logo_reader.c \
         ./lib/keylogger.c \
+        ./layer_state_reader.c \
+        #./lib/layer_state_reader.c \
         # ./lib/mode_icon_reader.c \
         # ./lib/host_led_state_reader.c \
         # ./lib/timelogger.c \
@@ -14,3 +15,4 @@ LTO_ENABLE = yes
 TAP_DANCE_ENABLE = yes
 
 BOOTMAGIC_ENABLE = lite
+MOUSEKEYS_ENABLE = no
