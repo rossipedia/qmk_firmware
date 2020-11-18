2d35e61fea903c8a8124a84c61fa1b8c7a3e6777
Bryan Ross <git@rossipedia.com>
2020-11-17 18:53:02 -0700

    make personal tap dance stuff conditional

--- users/rossipedia/rossipedia.c
+++ users/rossipedia/rossipedia.c
@@ -1,8 +1,10 @@
 #include QMK_KEYBOARD_H
 #include "rossipedia.h"
 
+#ifdef TAP_DANCE_ENABLE
 // Tap Dance Array
 qk_tap_dance_action_t tap_dance_actions[] = {
     [TD_OPT_SPACE] = ACTION_TAP_DANCE_DOUBLE(KC_LGUI, LALT(KC_SPC))
 };
+#endif
 
