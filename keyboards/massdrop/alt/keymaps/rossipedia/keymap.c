51f1d76ff613618ca619d6e88b763c4d36c842bc
Bryan Ross <git@rossipedia.com>
2021-01-05 21:17:52 -0700

    alt updates

--- keyboards/massdrop/alt/keymaps/rossipedia/keymap.c
+++ keyboards/massdrop/alt/keymaps/rossipedia/keymap.c
@@ -14,13 +14,15 @@ enum alt_layers {
     LAYER_FN = 3
 };
 
+#define KC_FN MO(LAYER_FN)
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [LAYER_BASE] = LAYOUT_65_ansi_blocker(
         KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC, KC_DEL,  \
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, KC_HOME, \
         CTL_ESC, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,  KC_PGUP, \
         KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,          KC_UP,   KC_PGDN, \
-        KC_ESC,  KC_LALT, KC_LGUI,                            KC_SPC,                             KC_RGUI, MO(LAYER_FN),   KC_LEFT, KC_DOWN, KC_RGHT  \
+        KC_FN,   KC_LALT, KC_LGUI,                            KC_SPC,                             KC_RGUI, KC_FN,   KC_LEFT, KC_DOWN, KC_RGHT  \
     ),
     [LAYER_WINDOWS] = LAYOUT_65_ansi_blocker(
         KC_ESC,  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
@@ -38,10 +40,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
     [LAYER_FN] = LAYOUT_65_ansi_blocker(
         KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, KC_INS,  \
-        _______, _______, KC_WIN,  _______, _______, _______, _______, _______, _______, _______, KC_PSCR, KC_SLCK, KC_PAUS, _______, KC_MUTE,\
-        _______, _______, _______, _______, _______, KC_GAM,  _______, _______, _______, _______, _______, _______,          _______, KC_VOLU, \
-        _______, _______, _______, _______, _______, MD_BOOT, _______, _______, _______, _______, _______, _______,          KC_PGUP, KC_VOLD, \
-        _______, _______, _______,                            _______,                            _______, _______, KC_HOME, KC_PGDN, KC_END  \
+        _______, _______, KC_WIN,  _______, _______, _______, _______, KC_HOME, KC_UP,   KC_END,  KC_PGUP, KC_PSCR, KC_SLCK, KC_PAUS, KC_MUTE,\
+        _______, _______, _______, _______, _______, KC_GAM,  _______, KC_LEFT, KC_DOWN, KC_RGHT, KC_PGDN, _______,          _______, KC_VOLU, \
+        _______, RGB_VAI, RGB_SAI, RGB_HUI, _______, MD_BOOT, _______, _______, _______, _______, _______, _______,          KC_PGUP, KC_VOLD, \
+        _______, _______, _______,                            RGB_TOG,                            _______, _______, KC_HOME, KC_PGDN, KC_END  \
     )
     /*
     [X] = LAYOUT(
