6f20389d8301c6ecbc98d05c71cfb6f41aebe800
Bryan Ross <git@rossipedia.com>
2022-04-01 21:44:12 -0600

    use "yes" instead of "lite" for bootmagic (lily58)

--- keyboards/lily58/keymaps/rossipedia/rules.mk
+++ keyboards/lily58/keymaps/rossipedia/rules.mk
@@ -1,4 +1,4 @@
-BOOTMAGIC_ENABLE = lite     # Virtual DIP switch configuration
+BOOTMAGIC_ENABLE = yes     # Virtual DIP switch configuration
 EXTRAKEY_ENABLE = yes       # Audio control and System control
 LTO_ENABLE = yes
 TAP_DANCE_ENABLE = yes
